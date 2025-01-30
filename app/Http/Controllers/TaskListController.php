4da84c219181f1184e8b09397aafb74173a96c14
nuroktavianiira <nuroktavianiira>
2025-01-30 10:50:33 +0700

    menambahkan tasklist controler

--- app/Http/Controllers/TaskListController.php
+++ app/Http/Controllers/TaskListController.php
@@ -2,9 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\TaskList;
 use Illuminate\Http\Request;
 
 class TaskListController extends Controller
 {
-    //
+    public function store(Request $request) {
+        $request->validate([
+            //user harus mengisi nama tidak lebih dari 100 hurup
+            'name' => 'required|max:100'
+        ]);
+
+        TaskList::create([
+            'name' => $request->name
+        ]);
+
+        return redirect()->back();
+    }
+
+    public function destroy($id) {
+        TaskList::findOrFail($id)->delete();
+
+        return redirect()->back();
+    }
 }

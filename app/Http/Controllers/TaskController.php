c90e5e994d9f1a3fd56dc88f525f92cfc3e9248c
nuroktavianiira <nuroktavianiira>
2025-02-13 13:36:35 +0700

    menghubungkan fitur update

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -1,56 +1,119 @@
 <?php
+// task controler digunakan untuk menampilkan isi daftar tugas,menambahkan tugas baru,mengedit tugas  dan menandai tugas selesai dan menghapus tugas //
 
+ // mengelompokkan kelas-kelas controller yang ada di dalam folder app/Http/Controllers.//
 namespace App\Http\Controllers;
-//mengambil data di app/model/task
+
+// untuk memanggil //
 use App\Models\Task;
 use App\Models\TaskList;
-use Illuminate\Http\Request;
+use Illuminate\Http\Request; 
+// untuk menangani permintaan data akses yg dikirim oleh penguna //
 
 class TaskController extends Controller
 {
     public function index() {
-        //untuk mengambil data fariable yang ada di dalam folder models/task
+        // berfungsi untuk mengambil data dari databse //
         $data = [
             'title' => 'Home',
-            //membuat judul untuk tampilan home 
-            'lists' => TaskList::all(),
-             //lists untuk mengambil semua taskList yang da di folder models/tasklist
-            'tasks' => Task::orderBy
-            ('created_at', 'desc')->get(),
-            //orderby adalah mengurutkan dari yang terbesar ke yang terkecil 
+            'lists' => TaskList::all(), 
+            // mengambil semua list yg ada di dalam database //
+            'tasks' => Task::orderBy('created_at', 'desc')->get(),
+            // descending dari besar ke kecil ascending:dari kecil ke besar //
+            // get mendapatkan data task yang nantinya akan dibuat berurutan sesuai urutan descending //
             'priorities' => Task::PRIORITIES
-            //untuk mengambil nilai priorities/kondisi dari yang ada di app/model/task
+            // priorities digunakan untuk mengambil data priority yg ada di database //
         ];
 
         return view('pages.home', $data);
+        // setelah kita mengambil data akan diarahkan ke halaman home //
     }
 
-    public function store(Request $request) {
-        $request->validate([
+    public function store(Request $request)
+    // digunakan untuk menyimpan data baru ke dalam databases // 
+     {
+        $request->validate
+        // digunakan untuk memvalidassi yg dikirim oleh pengguna //
+        ([
             'name' => 'required|max:100',
+            // required(wajib diisi)  max:100:tidak lebih dari 100 huruf//
+            
+            'description' => 'max:255' ,
+            'priority' =>  'required | in:low,medium,high'  ,
             'list_id' => 'required'
+            // list wajib diisi //
         ]);
 
         Task::create([
             'name' => $request->name,
+            'description' => $request->description, 
+            'priority' => $request->priority,
             'list_id' => $request->list_id
+            // membuat tugas baru
         ]);
 
 
         return redirect()->back();
+        // setelah menambhakan data task akan diarahkan kembali ke halaman home //
     }
 
-    public function complete($id) {
-        Task::findOrFail($id)->update([
+    public function complete($id)
+    // digunakan untuk membuat status tugas selesai //
+     {
+        Task::findOrFail($id)->update
+        // digunakan untuk memastikan tugas selsai //
+        ([
             'is_completed' => true
         ]);
 
         return redirect()->back();
     }
 
-    public function destroy($id) {
+    public function destroy($id)
+    // digunakan untuk menghapus tugas berdasarkan id //
+     {
         Task::findOrFail($id)->delete();
 
-        return redirect()->back();
+        return redirect()->route('home');
+    }
+    public function show($id)
+    {
+        $data = [
+            'title' => 'Task',
+            'lists' => TaskList::all(),
+            'task' => Task::findOrFail($id),
+        ];
+
+        return view('pages.details', $data);
+    }
+    public function changeList(Request $request, Task $task)
+    {
+        $request->validate([
+            'list_id' => 'required|exists:task_lists,id',
+        ]);
+
+        Task::findOrFail($task->id)->update([
+            'list_id' => $request->list_id
+        ]);
+
+        return redirect()->back()->with('success', 'List berhasil diperbarui!');
+    }
+    public function update(Request $request, Task $task)
+    {
+        $request->validate([
+            'list_id' => 'required',
+            'name' => 'required|max:100',
+            'description' => 'max:255',
+            'priority' => 'required|in:low,medium,high'
+        ]);
+
+        Task::findOrFail($task->id)->update([
+            'list_id' => $request->list_id,
+            'name' => $request->name,
+            'description' => $request->description,
+            'priority' => $request->priority
+        ]);
+
+        return redirect()->back()->with('success', 'Task berhasil diperbarui!');
     }
 }

d890ffee13ce9283348b2a2eb2cccd93c51678fd
nuroktavianiira <nuroktavianiira>
2025-02-14 10:41:02 +0700

    menambahkan fitur serch

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,10 +1,20 @@
 <nav class="navbar navbar-expand-lg bg-primary navbar-dark">
     {{-- nav class di gunakan untuk membungkus tampilan navbar --}}
-    {{-- navbar-expand-ig di gunakan untuk membuat tampilan navbar menjadi responsip--}}
+    {{-- navbar-expand-ig di gunakan untuk membuat tampilan navbar menjadi responsip --}}
     {{-- bg-primary di gunakan untuk mewarnai beground navbar menjadi berwarna biru --}}
     {{-- navbar-dark di gunakan untuk membuat teks navbar berwarna hitam --}}
     {{-- fixed-top di gunakan untuk untuk membut tataletak navbar menjadi tetap di atas --}}
-    <div class="container d-flex justify-content-center">
-        <a class="navbar-brand fw-bolder" href="#">{{ config('app.name') }}</a>
+    <a class="navbar-brand fw-bolder" href="#">{{ config('app.name') }}</a>
+
+    <div class="container d-flex justify-content-end">
+        <form class="d-flex" action="{{ route('search') }}" method="GET" role="search">
+            <!-- Input pencarian dengan id "search-input" -->
+            <input id="search-input" class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+            <!-- Tombol untuk melakukan pencarian atau refresh hasil pencarian
+             Formulir dengan input untuk mencari data, yang memungkinkan pengguna untuk mencari dan kemudian menampilkan hasil pencarian. Tombol bertuliskan "Refresh" digunakan untuk menyegarkan pencarian.-->
+            <button class="btn btn-outline-black" type="submit">Refresh</button>
+        </form>
+        
+
     </div>
 </nav>

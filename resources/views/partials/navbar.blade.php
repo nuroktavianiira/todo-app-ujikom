cae24e49fe62153a63c118bf393e64e041c18fea
nuroktavianiira <nuroktavianiira>
2025-02-16 00:20:24 +0700

    menghapus kode yang bersangkutan sama serch

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -4,17 +4,10 @@
     {{-- bg-primary di gunakan untuk mewarnai beground navbar menjadi berwarna biru --}}
     {{-- navbar-dark di gunakan untuk membuat teks navbar berwarna hitam --}}
     {{-- fixed-top di gunakan untuk untuk membut tataletak navbar menjadi tetap di atas --}}
-    <a class="navbar-brand fw-bolder" href="#">{{ config('app.name') }}</a>
+    <a class="navbar-brand fw-bolder" href="#">{{ config('app.name') }}
+    </a>
 
     <div class="container d-flex justify-content-end">
-        <form class="d-flex" action="{{ route('search') }}" method="GET" role="search">
-            <!-- Input pencarian dengan id "search-input" -->
-            <input id="search-input" class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-            <!-- Tombol untuk melakukan pencarian atau refresh hasil pencarian
-             Formulir dengan input untuk mencari data, yang memungkinkan pengguna untuk mencari dan kemudian menampilkan hasil pencarian. Tombol bertuliskan "Refresh" digunakan untuk menyegarkan pencarian.-->
-            <button class="btn btn-outline-black" type="submit">Refresh</button>
-        </form>
-        
 
     </div>
 </nav>

c0180734334e05065bd2e228df647296bd94cc64
nuroktavianiira <nuroktavianiira>
2025-02-17 19:25:10 +0700

    memasukan script untuk memanggil js dan untuk meng ngimport bootstrap

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,19 +13,22 @@
     <link rel="stylesheet" href="{{ asset('vendor/bootstrap/css/bootstrap.min.css') }}">
     <link rel="stylesheet" href="{{ asset('vendor/bootstrap-icons/font/bootstrap-icons.min.css') }}">
 </head>
-{{--body adalah wadah untuk menampilkan conten--}}
+{{-- body adalah wadah untuk menampilkan conten --}}
+
 <body>
     {{-- include di gunakan untuk mengambil pungsi pungsi komponen yang ada di dalam polder partial/napbar --}}
-    {{--body adalah wadah untuk menampilkan conten--}}
+    {{-- body adalah wadah untuk menampilkan conten --}}
     @include('partials.navbar') <!-- Mengambil component navbar -->
 
     @yield('content') <!-- Render content -->
-{{-- yield di gunakan untuk mendefinisikan tempat(section)didalam sebuah layout --}}
+    {{-- yield di gunakan untuk mendefinisikan tempat(section)didalam sebuah layout --}}
     @include('partials.modal')
-{{-- ingklud di gunakan untuk mengambil pungsi pungsi komponen yang ada di dalam polder partial/napbar --}}
+    {{-- ingklud di gunakan untuk mengambil pungsi pungsi komponen yang ada di dalam polder partial/napbar --}}
     <script src="{{ asset('js/script.js') }}"></script>
     <script src="{{ asset('vendor/bootstrap/js/bootstrap.min.js') }}"></script> <!-- Import bootstrap JS -->
     {{-- scrip untuk memanggil js --}}
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+
 </body>
 
 </html>
